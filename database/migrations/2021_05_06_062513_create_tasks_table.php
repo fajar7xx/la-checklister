46ebb327d92f0782f5e7fe06b9eeff4d88fabbcd
Fajar Siagian <fajar7xx@gmail.com>
2021-05-06 13:39:49 +0700

    ada saja

---
+++ database/migrations/2021_05_06_062513_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('checklist_id')->constrained();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('tasks');
+        Schema::enableForeignKeyConstraints();
+    }
+}
